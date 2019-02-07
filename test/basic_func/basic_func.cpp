b44953b508f11b8713f95cef9e68c9c66589636d
BSDinis <baltasardinis@hotmail.com>
2019-02-07 17:08:34 +0000

    added basic functionality tests

---
+++ test/basic_func/basic_func.cpp
@@ -0,0 +1,92 @@
+/**
+ * basic_func.cpp
+ *
+ * test if basic functionality is met:
+ *   - create & destroy
+ *   - add and remove edges
+ *   - add vertices
+ *   - return vertices
+ *   - print
+ */
+
+
+#include "graph.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+const int n_vertices = 10;
+const int n_edges_to_add = 20;
+const int n_biedges_to_add = 20;
+const int n_edges_to_rem = 4;
+const int n_biedges_to_rem = 6;
+
+// add vertices
+template <typename T>
+void test_1(graph<T> &g);
+
+// add edges
+template <typename T>
+void test_2(graph<T> &g);
+
+// return vertices
+template <typename T>
+void test_3(graph<T> &g);
+
+// remove edges
+template <typename T>
+void test_4(graph<T> &g);
+
+
+int main()
+{
+  graph<int> g;
+  std::srand(std::time(NULL));
+
+  test_1(g);
+  test_2(g);
+  test_3(g);
+  test_4(g);
+  std::cout << g;
+  return 0;
+}
+
+
+template <typename T>
+void test_1(graph<T> &g)
+{
+  std::cout << g;
+  for (int i = 0; i < n_vertices; i++)
+    g.add_vertex(std::rand());
+}
+
+template <typename T>
+void test_2(graph<T> &g)
+{
+  std::cout << g;
+  for (int i = 0; i < n_edges_to_add; i++)
+    g.add_edge(std::rand() % n_vertices, std::rand() % n_vertices);
+
+  for (int i = 0; i < n_biedges_to_add; i++)
+    g.add_biedge(std::rand() % n_vertices, std::rand() % n_vertices);
+}
+
+template <typename T>
+void test_3(graph<T> &g)
+{
+  std::cout << g;
+  for (int i = 0; i < n_vertices; i++)
+    std::cout << i << ": " << g.get_vertex(i) << std::endl;
+}
+
+template <typename T>
+void test_4(graph<T> &g)
+{
+  std::cout << g;
+  for (int i = 0; i < n_edges_to_rem; i++)
+    g.rem_edge(std::rand() % n_vertices, std::rand() % n_vertices);
+
+  for (int i = 0; i < n_biedges_to_rem; i++)
+    g.rem_biedge(std::rand() % n_vertices, std::rand() % n_vertices);
+}
+
